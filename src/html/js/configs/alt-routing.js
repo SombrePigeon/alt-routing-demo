bc6930c98f3d8cb3c3236af66c0bfc88c9f87f4f
SombrePigeon <contact@SombrePigeon.fr>
2024-11-27 01:21:37 +0100

    dynamic nav

--- src/html/js/configs/alt-routing.js
+++ src/html/js/configs/alt-routing.js
@@ -1,8 +1,9 @@
 import config from "alt-routing/config-base.js"
 
 const newConfig = structuredClone(config);
-newConfig.targetNavigation.origins = ["http://alt-routing-demo.sombrepigeon.fr"];
-newConfig.targetNavigation.targets = ["http://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.origins = ["https://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.targets = ["https://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.timout = 50;
 newConfig.route.localNav=true;
-newConfig.route.staticNav=true;
+newConfig.route.staticNav=false;
 export default newConfig;
