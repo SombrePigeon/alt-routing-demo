b58174dfa8b1ae61e8074cd1f6946f77c315fb11
SombrePigeon <contact@SombrePigeon.fr>
2024-11-27 08:31:11 +0100

    preload ressources

--- src/html/js/main.js
+++ src/html/js/main.js
@@ -2,6 +2,10 @@ import "alt-routing/route.js"
 import "alt-routing/anchor.js"
 import "alt-routing/router.js"
 import "alt-routing/title.js"
+/*preload*/
+import "alt-routing/config"
+import "alt-routing/config-base.js"
+import "alt-routing/namings.js"
 //import "dev";
 
 console.log("main");
