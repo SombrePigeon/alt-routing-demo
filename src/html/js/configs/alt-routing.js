061cfc63c07d1d4ad2f0f37a1c48db0bae0758b7
SombrePigeon <contact@SombrePigeon.fr>
2025-05-31 12:19:23 +0200

    Updates target navigations URLs to HTTPS
    prod is in https, update target navigation so its works in https

--- src/html/js/configs/alt-routing.js
+++ src/html/js/configs/alt-routing.js
@@ -1,8 +1,8 @@
 import config from "alt-routing/config-base.js"
 
 const newConfig = structuredClone(config);
-newConfig.targetNavigation.origins = ["http://alt-routing-demo.sombrepigeon.fr"];
-newConfig.targetNavigation.targets = ["http://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.origins = ["https://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.targets = ["https://alt-routing-demo.sombrepigeon.fr"];
 newConfig.targetNavigation.timout = 50;
 newConfig.route.useShadow = true;
 newConfig.route.style = "/css/shadowRoute.css";
