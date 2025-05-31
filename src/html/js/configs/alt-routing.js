a98f125b7997ab40d99666240ab1459e1c389b30
SombrePigeon <contact@SombrePigeon.fr>
2025-05-31 11:46:36 +0200

    new version of demo with some trasitions and selectors
    alt-routing 0.5.2

--- src/html/js/configs/alt-routing.js
+++ src/html/js/configs/alt-routing.js
@@ -1,9 +1,20 @@
 import config from "alt-routing/config-base.js"
 
 const newConfig = structuredClone(config);
-newConfig.targetNavigation.origins = ["https://alt-routing-demo.sombrepigeon.fr"];
-newConfig.targetNavigation.targets = ["https://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.origins = ["http://alt-routing-demo.sombrepigeon.fr"];
+newConfig.targetNavigation.targets = ["http://alt-routing-demo.sombrepigeon.fr"];
 newConfig.targetNavigation.timout = 50;
-newConfig.route.localNav=true;
-newConfig.route.staticNav=false;
+newConfig.route.useShadow = true;
+newConfig.route.style = "/css/shadowRoute.css";
+newConfig.route.localNav = true;
+newConfig.route.staticNav = true;
+newConfig.route.staticRouting = true;
+newConfig.route.showAttribute.locationMatch = true;
+newConfig.route.showAttribute.state = true;
+newConfig.route.showAttribute.status = true;
+newConfig.anchor.showAttribute.locationMatch = true;
+newConfig.routeur.features.shadowRouting = true;
+newConfig.routeur.features.styleShadowRouting = true;
+newConfig.routeur.features.updateTarget = true;
+newConfig.route.shadowRootInit.slotAssignment = "manual";
 export default newConfig;
--- src/html/js/main.js
+++ src/html/js/main.js
@@ -1,11 +1,20 @@
+//alt-routing submodules
+import "alt-routing/features/shadow.js"
+import "alt-routing/features/style.js"
+import "alt-routing/features/target.js"
+//alt-routing
 import "alt-routing/route.js"
 import "alt-routing/anchor.js"
 import "alt-routing/router.js"
 import "alt-routing/title.js"
+import "alt-routing/slot.js"
+//import "alt-routing/source.js"
+
+//import "alt-routing/source.js"
 /*preload*/
-import "alt-routing/config"
-import "alt-routing/config-base.js"
 import "alt-routing/namings.js"
+import "alt-routing/config-base.js"
+import "alt-routing/config"
 //import "dev";
 
 console.log("main");
